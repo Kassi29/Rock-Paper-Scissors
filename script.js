2956c6423086b6278a73f127517dbd9c64d4520a
Kass Badani <username@users.noreply.github.com>
2025-01-07 00:53:38 -0400

    Implement function that evaluate who has won

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const options = ["rock", "paper", "scissors"];
+let contComputer = 0;
+let contUser = 0;
+let computer;
+let user;
+let result;
 
 function computerPlay() {
   return options[Math.floor(Math.random() * 3)];
@@ -39,4 +44,26 @@ function rules(computer, user) {
   } else {
     return computer;
   }
+}
+
+function game() {
+  computer = computerPlay();
+  result = rules(computer, user);
+
+  if (result === "Computer") {
+    messages(computer, user, result);
+    contComputer++;
+  } else if (result === "User") {
+    messages(computer, user, result);
+    contUser++;
+  } else {
+    console.log("It's a draw");
+    console.log(`Both choose: ${result}`);
+  }
+  console.log(`Puntuation: Computer ${contComputer} - User ${contUser}`);
+}
+function messages(computer, user, result) {
+  console.log(`Computer choose: ${computer}`);
+  console.log(`User choose: ${user}`);
+  console.log(`${result} wins this round`);
 }
