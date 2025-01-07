c54ebca7b890ad4a45333542aa74425e3f94e488
Kass Badani <username@users.noreply.github.com>
2025-01-07 00:44:30 -0400

    Improve even more game's rules

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-console.log("Hello, World!");
-
-const options = ["rock", "papper", "scissors"];
+const options = ["rock", "paper", "scissors"];
 
 function computerPlay() {
   return options[Math.floor(Math.random() * 3)];
@@ -8,23 +6,35 @@ function computerPlay() {
 
 function userPlay() {
   let userChoice = prompt("Choose rock (1), paper (2) or scissors (3)");
+  switch (userChoice) {
+    case "1":
+      userChoice = "rock";
+      break;
+    case "2":
+      userChoice = "paper";
+      break;
+    case "3":
+      userChoice = "scissors";
+      break;
+  }
   return userChoice;
 }
 
 function rules(computer, user) {
   const point_1 = "Computer";
   const point_2 = "User";
-  if (computer === "rock" && user === 2) {
+
+  if (computer === "rock" && user === "paper") {
     return point_2;
-  } else if (computer === "rock" && user === 3) {
+  } else if (computer === "rock" && user === "scissors") {
     return point_1;
-  } else if (computer === "paper" && user === 1) {
+  } else if (computer === "paper" && user == "rock") {
     return point_1;
-  } else if (computer === "paper" && user === 3) {
+  } else if (computer === "paper" && user == "scissors") {
     return point_2;
-  } else if (computer === "scissors" && user === 2) {
+  } else if (computer === "scissors" && user === "paper") {
     return point_1;
-  } else if (computer === "scissors" && user === 1) {
+  } else if (computer === "scissors" && user === "rock") {
     return point_2;
   } else {
     return computer;
