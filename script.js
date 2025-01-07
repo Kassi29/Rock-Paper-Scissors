4eb781d7e63081fa7f9a0ee342ba98a6fa765f7d
Kass Badani <username@users.noreply.github.com>
2025-01-07 01:21:59 -0400

    Renamed some variables

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const options = ["rock", "paper", "scissors"];
-let contComputer = 0;
-let contUser = 0;
+let computerScore = 0;
+let humanScore = 0;
 let computer;
 let user;
 let result;
 
-function computerPlay() {
+function computerChoice() {
   return options[Math.floor(Math.random() * 3)];
 }
 
-function userPlay() {
+function humanChoice() {
   let userChoice = prompt("Choose rock (1), paper (2) or scissors (3)");
   switch (userChoice) {
     case "1":
@@ -30,7 +30,7 @@ function userPlay() {
   return userChoice;
 }
 
-function rules(computer, user) {
+function playRound(computer, user) {
   const point_1 = "Computer";
   const point_2 = "User";
 
@@ -52,20 +52,20 @@ function rules(computer, user) {
 }
 
 function game() {
-  computer = computerPlay();
-  result = rules(computer, user);
+  computer = computerChoice();
+  result = playRound(computer, user);
 
   if (result === "Computer") {
     messages(computer, user, result);
-    contComputer++;
+    computerScore++;
   } else if (result === "User") {
     messages(computer, user, result);
-    contUser++;
+    humanScore++;
   } else {
     console.log("It's a draw");
     console.log(`Both choose: ${result}`);
   }
-  console.log(`Puntuation: Computer ${contComputer} - User ${contUser}`);
+  console.log(`Puntuation: Computer ${computerScore} - User ${humanScore}`);
 }
 function messages(computer, user, result) {
   console.log(`Computer choose: ${computer}`);
@@ -74,7 +74,7 @@ function messages(computer, user, result) {
 }
 
 function start() {
-  user = userPlay();
+  user = humanChoice();
   if (user !== null) {
     if (user === "Invalid option") {
       alert("Invalid option");
@@ -83,15 +83,15 @@ function start() {
     initialMessages();
     game();
     bucle();
-    winnercont(contComputer, contUser);
+    winnercont(computerScore, humanScore);
   } else {
     console.log("The game has finished before");
   }
 }
 
 function bucle() {
-  while (contComputer < 3 && contUser < 3) {
-    user = userPlay();
+  while (computerScore < 5 && humanScore < 5) {
+    user = humanChoice();
     if (user !== null) {
       console.log("+++++++++++++++++ Other round +++++++++++++++++");
       if (user === "Invalid option") {
@@ -107,19 +107,19 @@ function bucle() {
 }
 function initialMessages() {
   console.log("Welcome to Rock, Paper, Scissors game");
-  console.log("The first player who wins 3 rounds is the winner");
+  console.log("The first player who wins 5 rounds is the winner");
   console.log("First round");
 }
 
-function winnercont(contComputer, contUser) {
+function winnercont(computerScore, humanScore) {
   console.log("///// Final Result /////");
-  if (contComputer > contUser) {
+  if (computerScore > humanScore) {
     console.log("Computer wins!!!");
-  } else if (contComputer < contUser) {
+  } else if (computerScore < humanScore) {
     console.log("User wins!!!");
   } else {
     console.log("It's a draw!!!");
-    console.log("Computer: " + contComputer + " - User: " + contUser);
+    console.log("Computer: " + computerScore + " - User: " + humanScore);
   }
 }
 
