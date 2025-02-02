0ee6a4cdc9a9009581eab829e2fa2f4fb322a172
Kass Badani <username@users.noreply.github.com>
2025-02-02 15:02:56 -0400

    Replace while loop with button click event for user interaction

--- script.js
+++ script.js
@@ -1,126 +1,136 @@
 const options = ["rock", "paper", "scissors"];
 let computerScore = 0;
 let humanScore = 0;
-let computer;
-let user;
+let computer, userChoice;
 let result;
+
+const results = document.querySelector("#results");
+const puntation = document.querySelector("#puntation");
+const btnRestart = document.querySelector("#restart");
+const all = document.getElementById("all");
+const winner = document.getElementById("winner");
+
+btnRestart.addEventListener("click", () => {
+  computerScore = 0;
+  humanScore = 0;
+  puntation.textContent = `0 : 0`;
+  clearMessages();
+  all.style.display = "block";
+  winner.textContent = "";
+});
+
+const btnGame = document.querySelector("#buttons");
+
+btnGame.addEventListener("click", (event) => {
+  let target = event.target;
+
+  switch (target.id) {
+    case "papper":
+      userChoice = "paper";
+      break;
+    case "rock":
+      userChoice = "rock";
+      break;
+    case "scissors":
+      userChoice = "scissors";
+      break;
+  }
+  game();
+});
 
 function computerChoice() {
   return options[Math.floor(Math.random() * 3)];
 }
 
-function humanChoice() {
-  let userChoice = prompt("Choose rock (1), paper (2) or scissors (3)");
-  switch (userChoice) {
-    case "1":
-      userChoice = "rock";
-      break;
-    case "2":
-      userChoice = "paper";
-      break;
-    case "3":
-      userChoice = "scissors";
-      break;
-    case null:
-      userChoice = null;
-      break;
-    default:
-      userChoice = "Invalid option";
+function playRound(computer, userChoice) {
+  if (computer === userChoice) {
+    return userChoice;
   }
-  return userChoice;
-}
 
-function playRound(computer, user) {
-  const point_1 = "Computer";
-  const point_2 = "User";
-
-  if (computer === "rock" && user === "paper") {
-    return point_2;
-  } else if (computer === "rock" && user === "scissors") {
-    return point_1;
-  } else if (computer === "paper" && user == "rock") {
-    return point_1;
-  } else if (computer === "paper" && user == "scissors") {
-    return point_2;
-  } else if (computer === "scissors" && user === "paper") {
-    return point_1;
-  } else if (computer === "scissors" && user === "rock") {
-    return point_2;
-  } else {
-    return computer;
+  if (
+    (computer === "rock" && userChoice === "paper") ||
+    (computer === "paper" && userChoice === "scissors") ||
+    (computer === "scissors" && userChoice === "rock")
+  ) {
+    return "User";
   }
+
+  return "Computer";
 }
 
 function game() {
+  clearMessages();
+
   computer = computerChoice();
-  result = playRound(computer, user);
+  result = playRound(computer, userChoice);
 
   if (result === "Computer") {
-    messages(computer, user, result);
+    messages(computer, userChoice, result);
     computerScore++;
   } else if (result === "User") {
-    messages(computer, user, result);
+    messages(computer, userChoice, result);
     humanScore++;
   } else {
-    console.log("It's a draw");
-    console.log(`Both choose: ${result}`);
+    const icon = document.createElement("i");
+
+    icon.classList.add(...getIconClass(result).split(" "));
+    results.textContent = `It's a draw!! Both choose:`;
+    results.appendChild(icon);
   }
-  console.log(`Puntuation: Computer ${computerScore} - User ${humanScore}`);
+  puntation.textContent = `${computerScore} : ${humanScore}`;
+
+  checkWinner();
 }
+
+function checkWinner() {
+  if (computerScore >= 3) {
+    all.style.display = "none";
+    winner.textContent = `COMPUTER WINS :(`;
+    setTimeout(() => {
+      btnRestart.click();
+    }, 4000);
+  } else if (humanScore >= 3) {
+    all.style.display = "none";
+
+    winner.textContent = `YOU WIN :)`;
+    setTimeout(() => {
+      btnRestart.click();
+    }, 4000);
+  }
+}
+
 function messages(computer, user, result) {
-  console.log(`Computer choose: ${computer}`);
-  console.log(`User choose: ${user}`);
-  console.log(`${result} wins this round`);
+  const line1 = document.createElement("span");
+  const line2 = document.createElement("span");
+  const line3 = document.createElement("span");
+
+  const compIcon = document.createElement("i");
+  const userIcon = document.createElement("i");
+
+  compIcon.classList.add(...getIconClass(computer).split(" "));
+  userIcon.classList.add(...getIconClass(user).split(" "));
+
+  line1.textContent = `Computer: `;
+  line1.appendChild(compIcon);
+  line2.textContent = `You:`;
+  line2.appendChild(userIcon);
+  line3.textContent = `${result} wins this round`;
+
+  results.appendChild(line1);
+  results.appendChild(line2);
+  results.appendChild(line3);
 }
 
-function start() {
-  user = humanChoice();
-  if (user !== null) {
-    if (user === "Invalid option") {
-      alert("Invalid option");
-      start();
-    }
-    initialMessages();
-    game();
-    bucle();
-    winnercont(computerScore, humanScore);
-  } else {
-    console.log("The game has finished before");
+function getIconClass(choice) {
+  switch (choice) {
+    case "rock":
+      return "fas fa-hand-rock";
+    case "paper":
+      return "fas fa-hand-paper";
+    case "scissors":
+      return "fas fa-hand-scissors";
   }
 }
-
-function bucle() {
-  while (computerScore < 5 && humanScore < 5) {
-    user = humanChoice();
-    if (user !== null) {
-      console.log("+++++++++++++++++ Other round +++++++++++++++++");
-      if (user === "Invalid option") {
-        alert("Invalid option");
-        bucle();
-      }
-      game();
-    } else {
-      console.log("The game has finished before");
-      break;
-    }
-  }
+function clearMessages() {
+  results.textContent = "";
 }
-function initialMessages() {
-  console.log("Welcome to Rock, Paper, Scissors game");
-  console.log("The first player who wins 5 rounds is the winner");
-  console.log("First round");
-}
-
-function winnercont(computerScore, humanScore) {
-  console.log("///// Final Result /////");
-  if (computerScore > humanScore) {
-    console.log("Computer wins!!!");
-  } else if (computerScore < humanScore) {
-    console.log("User wins!!!");
-  } else {
-    console.log("It's a draw!!!");
-    console.log("Computer: " + computerScore + " - User: " + humanScore);
-  }
-}
-
-//start();
